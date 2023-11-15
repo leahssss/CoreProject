4de439cca896ed806ab56c3495ae1f2228a21226
leahssss <150802415+leahssss@users.noreply.github.com>
2023-11-15 13:33:11 +0200

    Add files via upload

---
+++ task-core/Models/Task.cs
@@ -0,0 +1,15 @@
+namespace Models{
+    public class Task
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public bool status{ get; set; }
+    public string Deadline { get; set; }
+    public long AgentId { get; set; }
+    
+}
+}
+
+
+
+
